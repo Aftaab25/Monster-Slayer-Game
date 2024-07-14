4f7544c7dc84758458d03707458d06c4dc4d330b
aftaab25 <aftaab2507@gmail.com>
2024-07-15 01:56:01 +0530

    Add logic for Attack, Special Attack, Healing and Health Bars

---
+++ app.js
@@ -0,0 +1,51 @@
+function getRandomDamage(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  computed: {
+    playerHealthStyle() {
+      return { width: this.playerHealth + "%" };
+    },
+    monsterHealthStyle() {
+      return { width: this.monsterHealth + "%" };
+    },
+  },
+  methods: {
+    monsterAttack() {
+      monsterDamage = getRandomDamage(8, 12);
+      if (this.playerHealth - monsterDamage < 0) this.playerHealth = 0;
+      else this.playerHealth -= monsterDamage;
+    },
+    attack() {
+      playerDamage = getRandomDamage(6, 10);
+      if (this.monsterHealth - playerDamage < 0) this.playerHealth = 0;
+      else this.monsterHealth -= playerDamage;
+      this.monsterAttack();
+      console.log("Player Health: " + this.playerHealth);
+      console.log("Monster Health: " + this.monsterHealth);
+    },
+    specialAttack() {
+      playerSpecialDamage = getRandomDamage(9, 14);
+      if (this.monsterHealth - playerSpecialDamage < 0) this.playerHealth = 0;
+      this.monsterHealth -= playerSpecialDamage;
+      this.monsterAttack();
+      2;
+    },
+    heal() {
+      healing = getRandomDamage(10, 15);
+      if (this.playerHealth + healing > 100) this.playerHealth = 100;
+      else this.playerHealth += healing;
+      this.monsterAttack();
+    },
+    surrender() {},
+  },
+});
+
+app.mount("#game");
