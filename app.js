7e148e873246ab4ffa6f8533463035b1f4473920
aftaab25 <aftaab2507@gmail.com>
2024-07-17 00:04:03 +0530

    Add logic to Restart Game

--- app.js
+++ app.js
@@ -52,7 +52,11 @@ const app = Vue.createApp({
     surrender() {
       this.playerSurrender = true;
     },
-    restartGame() {},
+    restartGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.playerSurrender = false;
+    },
   },
 });
 
